f219789cc1963c412847c73f5098c8e014320f65
kianpas <kianpas@gmail.com>
2021-08-26 23:10:09 +0900

    2021.08.26 첫 커밋

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,43 @@
+//jsx가 아니므로 import react 불필요
+//CSS 모듈 사용시에는 style 주석처리, react 다시 사용
+import React from "react";
+//import styled from "styled-components";
+
+//css 모듈식 임포트
+ import styles from './Button.module.css';
+
+// const Button = styled.button`
+//   width : 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+
+//   @media (min-width : 768px){
+//     width : auto;
+//   }
+
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
+
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
